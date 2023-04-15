0b2efe3b0b3479bcfaa7885e1e73c9f12ab6046a
ByteDance <zhangyunnian@bytedance.com>
2023-04-15 18:30:02 +0800

    fix: 隐藏一些不必要的页面功能

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -13,7 +13,7 @@ import { ClearConversations } from './ClearConversations';
 import { PluginKeys } from './PluginKeys';
 
 export const ChatbarSettings = () => {
-  return null
+  return <div />
   const { t } = useTranslation('sidebar');
 
   const {
